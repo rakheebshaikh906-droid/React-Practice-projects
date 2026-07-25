ecf40e22a081dfa0f69d0fdc9a1b6368c3eb8554
Rakheeb <rakheebshaikh906@gmail.com>
2026-07-25 12:07:05 +0530

    added

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -1,4 +1,4 @@
-import Select from "./select";
+import Select from "./Select";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./container/Container";
