ec95db2864db556dc6cef8ea479523d5a66a3051
Rakheeb <rakheebshaikh906@gmail.com>
2026-07-25 11:58:17 +0530

    add footer

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -1,4 +1,4 @@
-import Select from "./Select";
+import Select from "./select";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./container/Container";
